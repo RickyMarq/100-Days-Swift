92764ae51171a785c9c2614aff6214435d23646d
Henrique Marques <henriquefmcosta75@gmail.com>
2022-02-12 22:26:19 -0300

    Dia 7

--- Dias 1 - 15/Dia 7.playground/Contents.swift
+++ Dias 1 - 15/Dia 7.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-// 
+// Usando closures como parâmetros que aceitam parâmetros.
+
+func travel (action: (String) -> Void) {
+    print("Estou pronto para ir")
+    action ("Londres")
+    print("Eu cheguei")
+}
+
+travel { (local: String) in
+    print("Eu vou para \(local) de avião")
+}
+
+
