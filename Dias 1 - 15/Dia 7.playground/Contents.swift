3a0089c0a89fa932c37296a1824cca6e13767119
Henrique Marques <henriquefmcosta75@gmail.com>
2022-02-14 20:22:54 -0300

    Dia 7

--- Dias 1 - 15/Dia 7.playground/Contents.swift
+++ Dias 1 - 15/Dia 7.playground/Contents.swift
@@ -12,15 +12,13 @@ travel { (local: String) in
     print("Eu vou para \(local) de avião")
 }
 
-// Quando usamos ? - 
+/* Usando closures como parâmetros quando retonram valores
+ 
+Estávamos usando Void que significa "Não aceita parâmetros e não retorna nada"
 
-// Usando closures como parâmetros quando retonram valores
+Podemos retirar o void e colocar qualquer tipo de dado para forçar ele a retornar um valor
 
-
-
-// Estávamos usando Void que significa "Não aceita parâmetros e não retorna nada.
-
-// Podemos retirar o void e colocar qualquer tipo de dado para forçar ele a retornar um valor
+*/
 
 func Pizza(action: (String) -> String) {
     print("Eu vou fazer uma pizza")
@@ -32,3 +30,63 @@ Pizza { (action: String) -> String in
     return "Eu fiz uma pizza de \(action)"
     
 }
+
+print("---Atalhos para funções---")
+
+Pizza {
+    ("Eu vou preparar uma pizza de \($0)")
+}
+
+// Ao invés de fazer todo o processo da linha 29 até 30 podemos fazer o da 36 até 38.
+
+// Nos dirigindo ao parâmetros com $0 até o parâmetro esperado.
+
+print("---Clousures com multiplos parâmetros---")
+
+func Pizza2(ação: (String, String) -> String ) {
+    print("Vou fazer outra Pizza")
+    let sabor = ação("Frango", "Catupiry")
+    print(sabor)
+}
+
+Pizza2 {
+    "Vou fazer uma pizza de \($0) com \($1)"
+}
+
+// Como vimos, nesta função temos 2 parâmetros além de termos chamando tal closure com o máximo de atalhos possíveis
+    
+print("---Retornando valores com funções---")
+
+// Do mesmo jeito como podemos passar closure em funções podemos retirar a closure dentro de uma função.
+
+// 1: -> Especifíca o retorno da função 2: -> Especifíca o retorno da clousure
+
+func viajar() -> (String) -> Void {
+    return {
+        print("Eu vou para \($0)")
+    }
+}
+
+// Agora podemos chamar a função para pegar a closure de volta aí chamar a função.
+
+var resultado = viajar()
+resultado("Austrália")
+
+print("---Capturando Valores---")
+
+// Se usarmos valores externos junto a uma closure o swift o guarda, mesmo que ele não exista mais.
+
+// A captura de valores ocorre quando criamos valores dentro de uma closure.
+
+func travel2() -> (String) -> Void {
+    var counter = 1
+    
+    return {
+        print("\(counter) eu vou para \($0)")
+        counter += 1
+    }
+}
+
+let result2 = travel2()
+result2("Londres")
+
