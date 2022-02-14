2fe443e5db5ca15c36e983aa3b934bd0b0e925d8
Henrique Marques <henriquefmcosta75@gmail.com>
2022-02-14 11:44:24 -0300

    Dia 7

--- Dias 1 - 15/Dia 6.playground/Contents.swift
+++ Dias 1 - 15/Dia 6.playground/Contents.swift
@@ -2,17 +2,27 @@ import UIKit
 
 // Closures
 
-// O Swift nos permite usar funções como qaulquer outro tipo com String ou Inteiros
+/* Closures são blocos de código que guardam funcionalidade e podemos re-passar em todo o código.
 
-let dirgigindo = {
-    print("Estou dirigindo")
+ A definição mais exata seria: "Aqui está um funcionalidade, mas não precisa usar agora"
+
+Usamos para:
+ 
+Rodar um código quando uma animação termina
+
+Rodar um código quando um download termina
+ 
+Roda um código quando o user selecionou uma opção no menu.
+
+*/
+
+let dirgigindo = { (lugar: String) in
+    print("Estou dirigindo até a \(lugar)")
 }
 
-// Nos permitindo chama-las como funções também
+dirgigindo("Praia")
 
-dirgigindo()
-
-// O nome disso é closures
+// Os parâmetros ficam dentro de chaves no clousures para não confundir o swift.
 
 // Closures que aceitem parâmetros
 
@@ -26,10 +36,46 @@ escutando("Radiohead", "Jigsaw falling into Place")
 
 // Retornando Valores com Closures
 
-let música = { (grupo: String, música: String) -> String in
-    return "Você ja ouviu \(música) ? ela é do \(grupo)"
+let música = { (grupo: String, album: String) -> String in
+    return "Você ja ouviu \(album) ? é um album do \(grupo)"
 }
 
-let menssagem = música("The Black Keys", "Chop and Change")
+let menssagem = música("The Black Keys", "Brothers")
 print(menssagem)
 
+// Caso queiramos criar uma função que retorne um valor, mas não aceite parâmetros fazemos.
+
+let pagamento = { () -> Bool in
+    if pagamento() {
+        print("Yay, finalmente")
+    } else {
+        print("Força, só mais alguns dias...")
+    }
+    return true
+}
+
+// Para que usamos closures como parâmetros
+
+/*
+
+ Quando fazemos algum pedido para a siri precisamos usar closures pois ao rodar o código e retornar uma closure o app fará isso com rapidez, não deixando a interface da siri travada como seria caso não fizessemos isto.
+ 
+ O mesmo para requisiões de internet, usamos closures para: "Quando terminar, rode isto"
+ 
+ Fazer isso nos permite não congelar a interface do usuário.
+
+*/
+
+// Trailing Closure
+
+func animate(duration: Double, animations: () -> Void ) {
+    print("Começando uma animação de \(duration) segundos")
+    animations()
+}
+
+// Trailing Closures nos permite deixar essa função que é bem comum mais enxuta
+
+animate(duration: 3, animations: {
+    print("Animação")
+
+})
--- Dias 1 - 15/Dia 7.playground/Contents.swift
+++ Dias 1 - 15/Dia 7.playground/Contents.swift
@@ -12,4 +12,23 @@ travel { (local: String) in
     print("Eu vou para \(local) de avião")
 }
 
+// Quando usamos ? - 
 
+// Usando closures como parâmetros quando retonram valores
+
+
+
+// Estávamos usando Void que significa "Não aceita parâmetros e não retorna nada.
+
+// Podemos retirar o void e colocar qualquer tipo de dado para forçar ele a retornar um valor
+
+func Pizza(action: (String) -> String) {
+    print("Eu vou fazer uma pizza")
+    let _ = action("Calabresa")
+    print("Eu fiz uma pizza de calabresa")
+}
+
+Pizza { (action: String) -> String in
+    return "Eu fiz uma pizza de \(action)"
+    
+}
