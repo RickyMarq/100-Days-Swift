08b48c5927acb37eaee816522066af00ddaefb59
Henrique Marques <henriquefmcosta75@gmail.com>
2022-02-18 23:13:51 -0300

    Dia 9

--- Dias 1 - 15/Dia 9.playground/Contents.swift
+++ Dias 1 - 15/Dia 9.playground/Contents.swift
@@ -14,3 +14,33 @@ struct User {
 }
 
  
+
+let tbk = [
+    "Drums": "Patrick",
+    "Voice": "Dan"
+]
+
+tbk["Drums"]
+
+let tbk2 = ["Dan", "Patrick"]
+
+tbk2[0]
+
+let aha = (Singer: "Morten", Guitarist: "Paul", Keyboardist: "Magne")
+
+print(aha)
+
+
+let quatro = 1..<5
+
+for number in quatro {
+    print("O número é \(number)")
+}
+
+
+let albums = ["Brothers", "Turn Blue"]
+
+for album in albums {
+    print("\(album) de The Black Keys está disponível no Apple Music")
+}
+    
